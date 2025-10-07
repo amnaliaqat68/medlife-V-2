fb3a0ad7f95da6e0bf58bf626f928c37039cf359
amnaliaqat68 <amnaliaqat7766@gmail.com>
2025-10-07 17:02:27 +0500

    updated

--- app/Users/addUsers/page.jsx
+++ app/Users/addUsers/page.jsx
@@ -256,7 +256,7 @@ export default function AddUserpage({ user, onSuccess }) {
           </div>
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Area (max 3)
+              Area (upto All)
             </label>
             <Select
               options={areaOptions}
@@ -265,7 +265,7 @@ export default function AddUserpage({ user, onSuccess }) {
                 form.district.includes(opt.value)
               )}
               onChange={(selected) => {
-                if (selected.length <= 3) {
+                if (selected.length <= 9) {
                   setForm({ ...form, district: selected.map((s) => s.value) });
                 }
               }}
